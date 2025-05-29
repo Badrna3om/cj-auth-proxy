26ee9dcca1f603911bcdd5ef3274a992225d172b
Badrna3om <Badrna3om@gmail.com>
2025-05-29 07:12:16 +0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+import requests
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'CJ Proxy is running.'
+
+@app.route('/token', methods=['GET'])
+def get_token():
+    url = "https://developers.cjdropshipping.com/authentication/getAccessToken"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "cjAccessKey": os.environ.get('CJ_ACCESS_KEY'),
+        "cjSecret": os.environ.get('CJ_SECRET')
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+    return jsonify(response.json())
+
+if __name__ == '__main__':
+    app.run()
